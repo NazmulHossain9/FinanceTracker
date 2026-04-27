a5491caee3c420bc2ceab4e1630f7a78929328e4
NazmulHossain9 <mn.cse48@gmail.com>
2026-04-27 16:50:28 +0600

    Add Transaction Dialog

---
+++ src/ui/AddTransactionDialog.h
@@ -0,0 +1,23 @@
+// AddTransactionDialog.h
+#pragma once
+#include <QDialog>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QDoubleSpinBox>
+#include <QDateEdit>
+
+class AddTransactionDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit AddTransactionDialog(QWidget* parent = nullptr);
+
+private slots:
+    void onAccept();
+
+private:
+    QLineEdit*      m_titleEdit;
+    QDoubleSpinBox* m_amountSpin;
+    QComboBox*      m_typeCombo;
+    QComboBox*      m_categoryCombo;
+    QDateEdit*      m_dateEdit;
+};
