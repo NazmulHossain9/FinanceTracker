f7f68a6cb5a717649a65977dde85fd46d673ca58
NazmulHossain9 <mn.cse48@gmail.com>
2026-04-27 16:48:50 +0600

    MainWindow UI

---
+++ src/ui/MainWindow.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <QMainWindow>
+#include <QTableWidget>
+#include <QLabel>
+#include <QPushButton>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private slots:
+    void onAddTransaction();
+    void onDeleteTransaction();
+    void refreshTable();
+    void updateSummary();
+
+private:
+    void setupUI();
+    void setupMenuBar();
+
+    QTableWidget* m_table;
+    QLabel*       m_balanceLabel;
+    QLabel*       m_incomeLabel;
+    QLabel*       m_expenseLabel;
+    QPushButton*  m_addBtn;
+    QPushButton*  m_deleteBtn;
+};
